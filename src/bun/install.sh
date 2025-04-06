5903d168ff2cd6f300992368b4e32f8cdad4831b
Hayden <haydenk@users.noreply.github.com>
2025-04-06 16:58:27 +0000

    fix install.

--- src/bun/install.sh
+++ src/bun/install.sh
@@ -4,6 +4,6 @@ set -e
 echo "Activating feature 'bun'"
 
 VERSION=${VERSION:-undefined}
-echo "The provided greeting is: $VERSION"
+echo "The provided version is: $VERSION"
 
-su $_REMOTE_USER_HOME curl -fsSL https://bun.sh/install | bash
+su $_REMOTE_USER curl -fsSL https://bun.sh/install | bash
