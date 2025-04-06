a7b0ebd50953edc0e5bea2e01011961823a16f9c
Hayden <haydenk@users.noreply.github.com>
2025-04-06 17:01:54 +0000

    set remote user in scenario and update install.sh

--- src/bun/install.sh
+++ src/bun/install.sh
@@ -6,4 +6,4 @@ echo "Activating feature 'bun'"
 VERSION=${VERSION:-undefined}
 echo "The provided version is: $VERSION"
 
-su $_REMOTE_USER curl -fsSL https://bun.sh/install | bash
+su -c $_REMOTE_USER 'curl -fsSL https://bun.sh/install | bash'
